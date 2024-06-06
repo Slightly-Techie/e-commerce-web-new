326d0c0cdc5843578cb94551be06257da2e355b4
Dornor Teye Israel <israelrankin@gmail.com>
2024-06-06 09:27:13 +0000

    nested navbar component

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./pages/App.tsx";
 import "./index.css";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<App />} />
         <Route
