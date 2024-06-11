bab1ce8d882b26b5e688b78d5b083feca3a5d22b
Dornor Teye Israel <israelrankin@gmail.com>
2024-06-11 14:52:43 +0000

    wrapped the navbar with the container component

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -9,6 +9,7 @@ import hamburgerIcon from "../../assets/icons/navbar/menu.svg";
 import personIcon from "../../assets/icons/navbar/person.svg";
 import MobileMenu from "./MobileMenu.tsx";
 import SearchBar from "./SearchBar.tsx";
+import Container from "../Container.tsx";
 
 const Navbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
@@ -23,36 +24,40 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="fixed left-0 top-0 z-40 flex w-full items-center justify-between p-4 bg-white">
-      <div className="flex w-full max-w-2xl items-center gap-4 md:gap-8 xl:max-w-3xl">
-        <Link to="/">
-          <img src={logo} alt="logo image" className="w-10" />
-        </Link>
+    <nav className="fixed left-0 top-0 z-40 flex w-full items-center justify-between bg-white p-4">
+      <Container>
+        <div className="flex items-center justify-between">
+          <div className="flex w-full max-w-2xl items-center gap-4 md:gap-8 xl:max-w-3xl">
+            <Link to="/">
+              <img src={logo} alt="logo image" className="w-10" />
+            </Link>
 
-        <div className="hidden w-full lg:block">
-          <SearchBar />
+            <div className="hidden w-full lg:block">
+              <SearchBar />
+            </div>
+          </div>
+
+          <div className="hidden items-center gap-4 lg:flex">
+            <NotificationIcon />
+            <CartIcon />
+            {isAuthenticated ? (
+              <Balance />
+            ) : (
+              <Button label="Sign In" icon={personIcon} />
+            )}
+          </div>
+
+          <MobileMenu
+            isOpen={isMobileMenuOpen}
+            isAuthenticated={isAuthenticated}
+            closeMenu={closeMobileMenuHandler}
+          />
+
+          <div className="lg:hidden" onClick={openMobileMenuHandler}>
+            <img src={hamburgerIcon} alt="menu icon" className="w-8" />
+          </div>
         </div>
-      </div>
-
-      <div className="hidden items-center gap-4 lg:flex">
-        <NotificationIcon />
-        <CartIcon />
-        {isAuthenticated ? (
-          <Balance />
-        ) : (
-          <Button label="Sign In" icon={personIcon} />
-        )}
-      </div>
-
-      <MobileMenu
-        isOpen={isMobileMenuOpen}
-        isAuthenticated={isAuthenticated}
-        closeMenu={closeMobileMenuHandler}
-      />
-
-      <div className="lg:hidden" onClick={openMobileMenuHandler}>
-        <img src={hamburgerIcon} alt="menu icon" className="w-8" />
-      </div>
+      </Container>
     </nav>
   );
 };
