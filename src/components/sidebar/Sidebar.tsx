10557579b3c69d253a19b66dcf345f7483e62678
Dornor Teye Israel <israelrankin@gmail.com>
2024-07-01 00:19:26 +0000

    Added notification badge to sidebar item

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,9 +1,10 @@
+import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
+import IndicatorIcon from "../../assets/icons/sidebar/Indicator.svg";
 import { SIDEBAR_LINKS } from "../../constants";
 import { cn } from "../../lib";
+import NotificationBadge from "../NotificationBadge";
 import STMember from "./STMember";
-import { useEffect, useState } from "react";
-import IndicatorIcon from '../../assets/icons/sidebar/Indicator.svg'
 
 const Sidebar = () => {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
@@ -13,61 +14,67 @@ const Sidebar = () => {
       setWindowWidth(window.innerWidth);
     };
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
   return (
-      <div
-        className={cn(
-          "fixed bottom-0 z-20 shadow-custom flex max-h-[896px] w-full lg:w-[260px] bg-white p-4 font-product-sans lg:left-auto lg:top-auto lg:h-custom-height lg:rounded-2xl lg:flex-col lg:bottom-auto lg:shadow-none",
-        )}
-      >
-        <ul className="flex justify-between w-full max-w-lg mx-auto lg:max-w-none lg:flex-col lg:gap-3">
-          {SIDEBAR_LINKS.map(({ route, label, label2, icon }, index) => {
-           if(windowWidth < 1024 && label === 'Favorites') return
+    <div
+      className={cn(
+        "fixed bottom-0 z-20 flex max-h-[896px] w-full bg-white p-4 font-product-sans shadow-custom lg:bottom-auto lg:left-auto lg:top-auto lg:h-custom-height lg:w-[260px] lg:flex-col lg:rounded-2xl lg:shadow-none",
+      )}
+    >
+      <ul className="mx-auto flex w-full max-w-lg justify-between lg:max-w-none lg:flex-col lg:gap-3">
+        {SIDEBAR_LINKS.map(({ route, label, label2, icon }, index) => {
+          if (windowWidth < 1024 && label === "Favorites") return;
 
-           return (
-             <li key={index}>
-               <NavLink
-                 to={route}
-                 className={({ isActive }) =>
-                   cn(
-                     isActive
-                       ? "font-bold text-[#181C20] lg:rounded-xl lg:border-gray200 lg:bg-gray100"
-                       : "text-gray500",
-                     "flex flex-col items-center gap-2.5 lg:flex-row lg:border lg:p-3 border-transparent",
-                   )
-                 }
-               >
-                 {({ isActive }) => (
-                   <>
-                     {icon}
-                     <span className="text-sm md:text-base">
-                       {windowWidth > 1024 ? label2 : null} {label}
-                     </span>
-                     <img
-                       src={IndicatorIcon}
-                       alt=""
-                       className={cn(
-                         "lg:hidden",
-                         !isActive ? "invisible" : "visible",
-                       )}
-                     />
-                   </>
-                 )}
-               </NavLink>
-             </li>
-           );
-          })}
-        </ul>
+          return (
+            <li key={index} className="relative">
+              <NavLink
+                to={route}
+                className={({ isActive }) =>
+                  cn(
+                    isActive
+                      ? "font-bold text-[#181C20] lg:rounded-xl lg:border-gray200 lg:bg-gray100"
+                      : "text-gray500",
+                    "flex flex-col items-center gap-2.5 border-transparent lg:flex-row lg:border lg:p-3",
+                  )
+                }
+              >
+                {({ isActive }) => (
+                  <>
+                    {icon}
+                    <span className="text-sm md:text-base">
+                      {windowWidth > 1024 ? label2 : null} {label}
+                    </span>
+                    <img
+                      src={IndicatorIcon}
+                      alt=""
+                      className={cn(
+                        "lg:hidden",
+                        !isActive ? "invisible" : "visible",
+                      )}
+                    />
+                  </>
+                )}
+              </NavLink>
 
-        <div className="hidden lg:block mt-auto">
+              {label === "Favorites" && (
+                <div className="absolute right-3 top-1/2 -translate-y-1/2">
+                  <NotificationBadge counter="6" />
+                </div>
+              )}
+            </li>
+          );
+        })}
+      </ul>
+
+      <div className="mt-auto hidden lg:block">
         <STMember />
-        </div>
       </div>
+    </div>
   );
 };
 
