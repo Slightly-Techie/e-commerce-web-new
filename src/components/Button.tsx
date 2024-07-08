efcaf5bf0c4dff3a02a50befc1eb05109422e30c
Dornor Teye Israel <israelrankin@gmail.com>
2024-07-08 16:46:33 +0000

    Modified Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,19 +6,23 @@ interface ButtonProps {
   label: string;
   icon?: string;
   secondary?: boolean;
+  white?: boolean;
   ghost?: boolean;
   invert?: boolean;
+  underline?: boolean;
   onClick?: () => void;
 }
 
 const Button: React.FC<ButtonProps> = ({
   className,
   label,
-  icon,
+  icon: Icon,
   secondary,
+  white,
   ghost,
   invert,
   onClick,
+  underline,
 }) => {
   return (
     <button
@@ -26,6 +30,8 @@ const Button: React.FC<ButtonProps> = ({
         "flex items-center justify-center gap-2.5 rounded-lg bg-[#111111] font-medium text-white hover:opacity-90",
         secondary && "bg-gray100",
         secondary && "text-[#111111]",
+        white && "bg-white",
+        white && "text-[#111111]",
         !ghost ? "px-4 py-2.5" : "px-0 py-0",
         ghost && "bg-transparent text-gray-500",
         invert && "flex-row-reverse",
@@ -33,7 +39,15 @@ const Button: React.FC<ButtonProps> = ({
       )}
       onClick={onClick}
     >
-      {icon && <img src={icon} alt="" />} {label}
+      <img src={Icon} alt="" />
+      <span
+        className={cn(
+          underline &&
+            'relative after:absolute after:bottom-0 after:right-0 after:h-[1px] after:w-full after:bg-gray500 after:content-[""]',
+        )}
+      >
+        {label}
+      </span>
     </button>
   );
 };
