586f55b6a4136ca5bb814580072cccda8f5c7247
saeedyussif663 <saeedyussif663@gmail.com>
2024-07-12 22:05:54 +0000

    explore slider

--- src/pages/explore/Explore.tsx
+++ src/pages/explore/Explore.tsx
@@ -1,9 +1,10 @@
+import ProductCategories from "@/components/ProductCategories";
 import PopularProducts from "./PopularProducts";
 
 const Explore = () => {
   return (
     <section className="rounded-tl-2xl rounded-tr-2xl bg-white px-6 py-[1.625rem] lg:ml-[276px]">
-      <span>Explore</span>
+      <ProductCategories />
 
       <PopularProducts />
     </section>
