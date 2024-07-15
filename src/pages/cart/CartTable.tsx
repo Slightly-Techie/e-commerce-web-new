e82abdbec8be1e197087a5c0dcffb80fe7c1d404
Dornor Teye Israel <israelrankin@gmail.com>
2024-07-15 08:29:55 +0000

    Created the cart table component

--- src/pages/cart/CartTable.tsx
+++ src/pages/cart/CartTable.tsx
@@ -1,35 +1,84 @@
-// import {
-//   Table,
-//   TableBody,
-//   TableCaption,
-//   TableCell,
-//   TableHead,
-//   TableHeader,
-//   TableRow,
-// } from "@/components/ui/tabel"
+import Asus from "@/assets/images/products/Asus.png";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import CartItem from "./CartItem";
+import QuantityControl from "./QuantityControl";
+
+const items = [
+  {
+    id: "1",
+    name: 'Q-base 45" wide monitor...',
+    image: Asus,
+    amount: 3,
+    unit_price: "4530.00",
+    final_price: "4530.00",
+  },
+  {
+    id: "2",
+    name: 'Q-base 45" wide monitor...',
+    image: Asus,
+    amount: 3,
+    unit_price: "4530.00",
+    final_price: "4530.00",
+  },
+  {
+    id: "2",
+    name: 'Q-base 45" wide monitor...',
+    image: Asus,
+    amount: 3,
+    unit_price: "4530.00",
+    final_price: "4530.00",
+  },
+];
 
 const CartTable = () => {
   return (
-    // <Table>
-    //   <TableCaption>A list of your recent invoices.</TableCaption>
-    //   <TableHeader>
-    //     <TableRow>
-    //       <TableHead className="w-[100px]">Invoice</TableHead>
-    //       <TableHead>Status</TableHead>
-    //       <TableHead>Method</TableHead>
-    //       <TableHead className="text-right">Amount</TableHead>
-    //     </TableRow>
-    //   </TableHeader>
-    //   <TableBody>
-    //     <TableRow>
-    //       <TableCell className="font-medium">INV001</TableCell>
-    //       <TableCell>Paid</TableCell>
-    //       <TableCell>Credit Card</TableCell>
-    //       <TableCell className="text-right">$250.00</TableCell>
-    //     </TableRow>
-    //   </TableBody>
-    // </Table>
-    <p>Table</p>
+    <Table className="font-semibold text-[#111111]">
+      <TableHeader>
+        <TableRow className="text-base">
+          <TableHead className="font-semibold text-[#111111]">
+            Product
+          </TableHead>
+          <TableHead className="text-center font-semibold text-[#111111]">
+            Unit Price
+          </TableHead>
+          <TableHead className="text-center font-semibold text-[#111111]">
+            Quantity
+          </TableHead>
+          <TableHead className="text-center font-semibold text-[#111111]">
+            Final Price
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+
+      <TableBody>
+        {items.map((item) => (
+          <TableRow className="text-base" key={item.id}>
+            <TableCell className="min-w-[340px]">
+              <CartItem {...item} />
+            </TableCell>
+
+            <TableCell className="min-w-[109px] text-center">
+              {item.unit_price}
+            </TableCell>
+
+            <TableCell className="min-w-[126px]">
+              <QuantityControl amount={item.amount} />
+            </TableCell>
+
+            <TableCell className="min-w-[109px] text-center">
+              {item.final_price}
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
   );
 };
 
