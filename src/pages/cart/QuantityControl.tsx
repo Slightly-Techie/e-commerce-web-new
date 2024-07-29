9dcfca7694b6437efd1d7177c76e48ae7a8e84b0
Dornor Teye Israel <israelrankin@gmail.com>
2024-07-29 09:24:24 +0000

    updated the component

--- src/pages/cart/QuantityControl.tsx
+++ src/pages/cart/QuantityControl.tsx
@@ -1,33 +1,45 @@
 import minusIcon from "@/assets/icons/cart/minus.svg";
 import plusIcon from "@/assets/icons/cart/plus.svg";
+import { cn } from "@/lib";
+import { useCartStore } from "@/store/cartStore";
 import { useState } from "react";
 
 interface QuantityControlProps {
+  id: string;
   amount: number;
+  btn?: boolean;
 }
 
-const QuantityControl = ({ amount }: QuantityControlProps) => {
+const QuantityControl = ({ id, amount, btn }: QuantityControlProps) => {
   const [quantity, setQuantity] = useState(amount);
+  const addToCart = useCartStore((state) => state.addItem);
+  const removeFromCart = useCartStore((state) => state.removeItem);
 
   const increaseQuantityHandler = () => {
     setQuantity((prev) => prev + 1);
+    addToCart({ id });
   };
 
   const decreaseQuantityHandler = () => {
-    if (quantity <= 1) return;
-
     setQuantity((prev) => prev - 1);
+    removeFromCart(id);
   };
 
   return (
-    <div className="mx-auto flex w-fit gap-3.5">
+    <div
+      className={cn(
+        "mx-auto flex w-fit gap-3.5",
+        btn &&
+          "w-full justify-between gap-0 rounded-md border border-gray-200 px-4 py-2.5",
+      )}
+    >
       <img
         src={minusIcon}
         alt=""
         className="cursor-pointer hover:opacity-90"
         onClick={decreaseQuantityHandler}
       />
-      <span>{quantity}</span>
+      <span className="font-bold">{quantity}</span>
       <img
         src={plusIcon}
         alt=""
