4eb673b5a05d7071cadfe6356fe4e3480e82d2b9
Dornor Teye Israel <israelrankin@gmail.com>
2024-07-29 09:23:48 +0000

    Displaying the total price of the cart

--- src/pages/cart/OrderTotal.tsx
+++ src/pages/cart/OrderTotal.tsx
@@ -1,7 +1,10 @@
 import alertCircleIcon from "@/assets/icons/alert-circle.svg";
 import Button from "@/components/Button";
+import { useCartStore } from "@/store/cartStore";
 
 const OrderTotal = () => {
+  const totalPrice = useCartStore((state) => state.totalPrice);
+
   return (
     <div className="rounded-[14px] border border-gray200 bg-white p-6 lg:w-[449px]">
       <h3 className="mb-[1.875rem] border-b border-b-gray-200 pb-[1.875rem] text-xl font-bold">
@@ -18,17 +21,17 @@ const OrderTotal = () => {
       <div className="grid gap-[1.875rem]">
         <div className="flex items-center justify-between text-gray500">
           <span>Subtotal</span>
-          <span>Ghc 45088.00</span>
+          <span>₵{totalPrice}</span>
         </div>
 
         <div className="flex items-center justify-between text-gray500">
           <span>Discount</span>
-          <span>-Ghc 0.00</span>
+          <span>-₵0.00</span>
         </div>
 
         <div className="flex items-center justify-between text-lg font-bold">
           <span>Total</span>
-          <span>Ghc 4,600.00</span>
+          <span>₵{totalPrice.toFixed(2)}</span>
         </div>
 
         <Button label="Check out now" className="font-semibold" />
