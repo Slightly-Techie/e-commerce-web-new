3eaf406b6698e256cd98f99c75a35934958a1b0f
Dornor Teye Israel <israelrankin@gmail.com>
2024-07-29 09:22:10 +0000

    added functionalities of toggling favorite and removing the item from the cart

--- src/pages/cart/CartItem.tsx
+++ src/pages/cart/CartItem.tsx
@@ -1,6 +1,9 @@
 import heartIcon from "@/assets/icons/cart/heart-gray.svg";
+import heartFavIcon from "@/assets/icons/cart/heart-red.svg";
 import trashIcon from "@/assets/icons/cart/trash-gray.svg";
 import Button from "@/components/Button";
+import useFavorite from "@/hooks/useFavorite";
+import { useCartStore } from "@/store/cartStore";
 
 interface CartItemProps {
   id: string;
@@ -9,10 +12,14 @@ interface CartItemProps {
 }
 
 const CartItem = ({ id, image, name }: CartItemProps) => {
+  const { isFavorite, toggleFavoriteHandler } = useFavorite();
+
+  const removeFromCart = useCartStore((state) => state.removeItem);  
+
   return (
     <div className="flex items-center gap-4">
       <div className="grid h-[92px] w-[92px] place-items-center rounded-[7px] bg-gray100">
-        <img src={image} alt="" className="h-[62px] w-[77px]" />
+        <img src={image} alt="" className="h-[45px] w-[60px]" />
       </div>
 
       <div>
@@ -20,15 +27,19 @@ const CartItem = ({ id, image, name }: CartItemProps) => {
         <div className="flex items-center gap-4">
           <Button
             label="Save item"
-            icon={heartIcon}
+            icon={!isFavorite ? heartIcon : heartFavIcon}
             ghost
             className="w-fit gap-1.5 text-sm text-gray400"
+            onClick={toggleFavoriteHandler}
           />
           <Button
             label="Remove item"
             icon={trashIcon}
             ghost
             className="w-fit gap-1.5 text-sm text-gray400"
+            onClick={() => {
+              removeFromCart(id, true);              
+            }}
           />
         </div>
       </div>
