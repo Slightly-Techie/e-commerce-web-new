88be1c41a30d82b1c097ca73ef633738a47bc821
Dornor Teye Israel <israelrankin@gmail.com>
2024-08-09 14:33:26 +0000

    created DeliveryAddress component

---
+++ src/pages/checkout/steps/delivery-address/DeliveryAddress.tsx
@@ -0,0 +1,70 @@
+import Input from "@/components/Input";
+import { FC } from "react";
+import UserLocation from "./UserLocation";
+import { FieldErrors, UseFormRegister } from "react-hook-form";
+import { FormValues } from "../CheckoutSteps";
+
+
+
+interface DeliveryAddressProps {
+  register: UseFormRegister<FormValues>;
+  errors: FieldErrors<FormValues>;
+}
+
+const DeliveryAddress: FC<DeliveryAddressProps> = ({ register, errors }) => {
+  return (
+    <>
+      <form className="mb-12">
+        <div className="grid gap-6 md:grid-cols-2 md:gap-8">
+          <Input
+            register={register}
+            errors={errors}
+            id="firstname"
+            label="First Name"
+            placeholder="Eg James"
+            required
+          />
+
+          <Input
+            register={register}
+            errors={errors}
+            id="lastname"
+            label="Last Name"
+            placeholder="Eg Zimbi"
+            required
+          />
+
+          <Input
+            register={register}
+            errors={errors}
+            id="number"
+            label="Number"
+            type="number"
+            placeholder="+233 56 789 5678"
+            required
+          />
+
+          <div>
+            <Input
+              register={register}
+              errors={errors}
+              id="email"
+              label="Email Address"
+              type="email"
+              placeholder="Eg st@mail.com"
+              required
+            />
+
+            <span className="mt-1 text-xs text-gray-500">
+              Invoice is sent to this provided email
+            </span>
+          </div>
+        </div>
+      </form>
+
+      <UserLocation />
+    </>
+  );
+};
+
+export default DeliveryAddress;
