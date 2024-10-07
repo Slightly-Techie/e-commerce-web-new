27da2a111e04124db20a4b9502add5d6a7872e6c
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-10-07 19:57:11 +0000

    Update main entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import "./index.css";
-import App from "./pages/App.tsx";
+import AppRouter from "./pages/AppRouter.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={AppRouter} />
   </React.StrictMode>,
 );
