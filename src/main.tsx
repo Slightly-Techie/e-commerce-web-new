2cafb176713293ade4726badb1d2653308b25e78
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-10-09 18:42:36 +0000

    Modify main entry point for application enhancements

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { CookiesProvider } from "react-cookie";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { Toaster } from "./components/ui/sonner.tsx";
 import "./index.css";
 import AppRouter from "./pages/AppRouter.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={AppRouter} />
+    <>
+      <CookiesProvider defaultSetOptions={{ path: "/" }}>
+        <RouterProvider router={AppRouter} />
+      </CookiesProvider>
+      <Toaster />
+    </>
   </React.StrictMode>,
 );
