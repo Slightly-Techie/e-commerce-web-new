58686df90264293b8c8db86129735e9006538032
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-10-09 18:33:29 +0000

    Updated Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,39 @@
 import react from "@vitejs/plugin-react";
+import http from "https";
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), svgr()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react(), svgr()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  server: {
-    port: 3000,
-  },
+    server: {
+      //   https: {
+      //     key: fs.readFileSync("./.cert/key.pem"),
+      //     cert: fs.readFileSync("./.cert/cert.pem"),
+      //   },
+      port: 3000,
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL,
+          secure: false,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+          agent: new http.Agent({ rejectUnauthorized: false }),
+          configure: (proxy) => {
+            proxy.on("error", (err) => {
+              console.error(err);
+            });
+          },
+        },
+      },
+    },
+  };
 });
