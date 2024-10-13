08a9be9707efb66c03bf10a28152ade81c3a348d
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2024-10-13 20:15:02 +0000

    add icon and loader

--- src/components/checkout/st_member_checkout.tsx
+++ src/components/checkout/st_member_checkout.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { CircleCheck } from 'lucide-react'
+import { ClipLoader } from 'react-spinners' // Import the spinner component
 
 type Props = {}
 type userDetailsProps = {
@@ -30,12 +32,15 @@ export default function St_member_checkout({ }: Props) {
             <h1 className='font-bold text-2xl text-center'>Setting up account</h1>
             <p className='text-gray-400 text-center'>Importing your details from CRM. This may take a few minutes...</p>
             <div className='w-[300px] border-black border-[1px] rounded px-5 py-6'>
-
                 {userDetails.map((user, index) => (
                     <div key={index} className='flex justify-between py-2 border-b '>
                         <h1 className='font-semibold'>{user.title}</h1>
                         <p className={user.details ? 'text-blue-500' : 'text-gray-500'}>
-                            {user.details ? "Loaded" : "Loading..."}
+                            {user.details ? <CircleCheck size={20} /> : 
+                                <div className='h-[18px] w-[18px]'>
+                                    <ClipLoader size={20} color={"#000"} loading={true} />
+                                </div>
+                            }
                         </p>
                     </div>
                 ))}
