679ca8296053cdb85c6446adb430b01bea4bf7d3
Zerebel <86872654+Zerebel@users.noreply.github.com>
2025-03-24 21:19:32 +0000

    Refactor import paths in VerifySignupCode component to use absolute imports for improved clarity and maintainability.

--- src/pages/auth/VerifySignupCode.tsx
+++ src/pages/auth/VerifySignupCode.tsx
@@ -1,7 +1,7 @@
-import VerifyCodeForm from "../components/VerifyCodeForm";
+import VerifyCodeForm from "@/components/VerifyCodeForm";
 
-import AuthLayout from "../components/AuthLayout";
-import SetupAccountRouter from "../components/SetupAccountRouter";
+import AuthLayout from "@/components/AuthLayout";
+import SetupAccountRouter from "@/components/SetupAccountRouter";
 
 const VerifySignupCode = () => {
   return (
